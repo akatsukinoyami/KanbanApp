afae69406f052534c3a20fb74fa51c555a78914b
akatsukinoyami <hirokod3@gmail.com>
2023-05-25 00:31:57 +0300

    add relation and enums to tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
-class Task < ApplicationRecord
+class Task
+    belong_to :user
+
+    enum status: { to_do: 0, in_progress: 1, review: 2, done: 3, cancelled: 4 }
+    enum priority: { high: 0, normal: 1, low: 1 }
 end
