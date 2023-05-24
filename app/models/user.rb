5610a292a2316e93cc5a3f049728388daf2c1ff6
akatsukinoyami <hirokod3@gmail.com>
2023-05-25 00:31:05 +0300

    add relation and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable :recoverable, 
-  # :rememberable, and :omniauthable
+class User
+  has_many :tasks
   
+  validates :first_name, presence: true
+  validates :email, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i },  uniqueness: { case_sensitive: false }
+
   devise :database_authenticatable, :registerable, :validatable
 end
