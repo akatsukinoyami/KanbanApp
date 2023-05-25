484e8e28e6b8a5532a975f9ab0df6d2661865f3d
akatsukinoyami <hirokod3@gmail.com>
2023-05-25 21:46:40 +0300

    add task log preparation to model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,50 @@
 class Task < ApplicationRecord
-    scope :with_status, ->(status) { where(status:) }
-    scope :with_priority, ->(priority) { where(priority:) }
+  scope :with_status, ->(status) { where(status:) }
+  scope :with_priority, ->(priority) { where(priority:) }
 
-    enum status: { to_do: 0, in_progress: 1, review: 2, done: 3, cancelled: 4 }
-    enum priority: { high: 0, normal: 1, low: 2 }
-    
-    belongs_to :user
+  enum status: { to_do: 0, in_progress: 1, review: 2, done: 3, cancelled: 4 }
+  enum priority: { high: 0, normal: 1, low: 2 }
+  
+  belongs_to :user
 
-    audited
+  audited
+
+  def logs_humanized
+    self.audits
+      .reorder(created_at: :desc)
+      .pluck(:id, :created_at, :action, :audited_changes)
+      .map do |(id, created_at, action, audited_changes)|
+      {
+        id:,
+        created_at:,
+        action: action.humanize,
+        audited_changes: self.logs_humanize!(audited_changes),
+      }
+    end 
+  end
+
+  private
+
+  def logs_humanize! audited_changes
+    audited_changes.map do |column_name, content|
+      if content.kind_of?(Array)
+        (from, to) = content
+        from = log_fields_humanize column_name, from
+        to = log_fields_humanize column_name, to
+      else
+        from = nil
+        to = log_fields_humanize column_name, content
+      end
+      [column_name.humanize, from, to]
+    end
+  end
+
+  def log_fields_humanize column_name, content
+    case column_name
+      when "status"   then Task.statuses.keys[content].humanize
+      when "priority" then Task.priorities.keys[content].humanize
+      when "user_id"  then User.find_by(id: content).name
+      else content
+    end
+  end
 end
