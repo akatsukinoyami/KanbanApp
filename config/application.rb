92b61b47be9bee41dc3d8d5c3cfbdf5fd6d08a8c
akatsukinoyami <hirokod3@gmail.com>
2023-05-27 03:56:48 +0300

    fixed problem with assets

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,5 @@ module KanbanApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    
-    config.hosts << ENV["ALLOWED_HOST"]
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,16 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  pp ENV
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"]
+  config.serve_static_assets = ENV["RAILS_SERVE_STATIC_FILES"]
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.compress = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -90,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.hosts << ENV["ALLOWED_HOST"]
 end
